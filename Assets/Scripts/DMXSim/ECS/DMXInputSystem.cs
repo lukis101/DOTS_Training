896415827d690fc7141ecae9e562d5fe5c1fa24a
Lukas Jonyla <lukas.jonyla@unity3d.com>
2020-02-19 11:22:24 +0200

    Input system and fixture component ECS classes

---
+++ Assets/Scripts/DMXSim/ECS/DMXInputSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class DMXInputSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var dtime = Time.DeltaTime;
+        var outputDeps = Entities.WithoutBurst().ForEach((ref Rotation rot, ref DMXFixtureRotatorComponent comp) => {
+            float curangle = comp.CurrentAngle;
+            curangle += dtime * 40f;
+            quaternion rot2 = Quaternion.Euler(rot.Value.value.x, curangle, rot.Value.value.y);
+            rot = new Rotation { Value = rot2 };
+            comp.CurrentAngle = curangle;
+        }).Schedule(inputDeps);
+
+        return outputDeps;
+    }
+}
