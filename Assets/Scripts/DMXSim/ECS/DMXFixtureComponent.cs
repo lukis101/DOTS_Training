95b31ddb898c12e42483ff162b35324b01ec02de
Lukas Jonyla <lukas.jonyla@unity3d.com>
2020-02-19 16:03:49 +0200

    ECS dependency tests

---
+++ Assets/Scripts/DMXSim/ECS/DMXFixtureComponent.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct DMXFixtureComponent : IComponentData
+{
+    public int address;
+    public int universe;
+    public Entity parts;
+}
---
+++ Assets/Scripts/DMXSim/ECS/DMXParameterComponent.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+//[GenerateAuthoringComponent]
+public struct DMXParameterComponent : IComponentData
+{
+    public int Address;
+    public int Universe;
+    public int MinValue;
+    public int MaxValue;
+    public float CurValue;
+}
